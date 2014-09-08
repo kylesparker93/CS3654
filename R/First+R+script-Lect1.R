fa66b9934089fec63f965a3e7a9553638b156698
kylesparker93 <kylesparker93@gmail.com>
2014-09-08 00:09:17 -0400

    Made work
    for mac

--- R/First+R+script-Lect1.R
+++ R/First+R+script-Lect1.R
@@ -13,8 +13,8 @@ total <- 1+2
 total
 
 #Where is everything going to be saved?
-#setwd("C:\Users\Denisa\Documents\Rfolder")-this works on MACS
-setwd("C:\\Users\\Denisa\\Documents\\Rfolder") #need double backslash on PC
+setwd("C:\Users\Denisa\Documents\Rfolder")-this works on MACS
+#setwd("C:\\Users\\Denisa\\Documents\\Rfolder") #need double backslash on PC
 help.start()
 
 ?setwd
