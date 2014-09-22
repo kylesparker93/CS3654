0e314d9ad8addd69ce054b278613827dfd84e514
kylesparker93 <kylesparker93@gmail.com>
2014-09-21 22:35:14 -0400

    Inclass 2 and 3
    Apparently never committed in class 2, and also now commiting in class
    #3

--- R/Inclass2/Inclass2.R
+++ R/Inclass2/Inclass2.R
@@ -26,8 +26,36 @@ summary(d$Other.debtors)
 # We also get no information from this column
 # We can conclude the german credit data will not be easy to work with and the values are arbitrary and not clear
 
+install.packages("ggplot2", dependencies=TRUE)
+library("ggplot2")
+custdata2 <- subset(custdata,
+                    (custdata$age > 0 & custdata$age < 100
+                     & custdata$income > 0))
+cor(custdata2$age, custdata2$income)
+
+ggplot(custdata2, aes(x=age,y=income)) +
+  geom_point() +
+  ylim(0,200000) +
+  theme_bw() +
+  ggtitle("Scatterplot of Income vs Age")
 
 install.packages("hexbin")
 library("hexbin")
-help(package=hexbin)
-hexbinplot(custdata$age, custdata$income)
+ggplot(custdata2, aes(x=age, y=income)) +
+  geom_hex(binwidth=c(5,10000)) +
+  theme_bw() +
+  ylim(0, 200000)
+# The hexbin looks relatively similar to the scatterplot in shape and density
+
+ggplot(custdata2, aes(x=num.vehicles,y=income)) +
+  geom_point() +
+  ylim(0,200000) +
+  geom_smooth() + 
+  theme_bw() +
+  ggtitle("Scatterplot of Income vs Age")
+# I opted to use a scatterplot with a smoothed curve to show the correlation, which is loose
+
+ggplot(custdata2) + 
+  geom_bar(aes(x=income.lt.30K, fill=recent.move))
+# I opted to use a bar chart. It seems as if there is a small correlation between income
+# below 30k and recent moves, however the data is not complete.
